06f2bb4fd800081daba0fedc43cf5d4c35e15e39
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2021-04-21 19:58:37 -0500

    add new class

---
+++ src/models/NBClassifier.py
@@ -0,0 +1,15 @@
+import base_models
+import numpy as pd
+import pandas as pd
+
+
+class NBClassifier(base_models):
+
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
