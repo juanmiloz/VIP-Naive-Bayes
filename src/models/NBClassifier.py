8f8822cb1b87c38308ef842729c2b784a746c609
Juan Camilo Zorrilla <juanmiloz@hotmail.com>
2021-05-19 19:23:20 -0500

    add code to Naive bayes

--- src/models/NBClassifier.py
+++ src/models/NBClassifier.py
@@ -1,5 +1,5 @@
 import base_models
-import numpy as pd
+import numpy as np
 import pandas as pd
 
 
@@ -9,7 +9,40 @@ class NBClassifier(base_models):
         pass
 
     def fit(self, X, y):
-        pass
+        numSamples, numFeatures = X.shape #assigment to numSamples and numFeature the value of shape
+        self._classes = np.unique(y) #obtain the unique value for each classes
+        numClasses = len(self._classes) #assigment the length to classes
 
+        #np.float64 same float but an accurate representation of the values
+        #np.zeros creates an array of (n,m) size, and typenp.float64 and fills it with ceros
+        self._mean = np.zeros((numClasses,numFeatures), dtype=np.float64)
+        self._var = np.zeros((numClasses, numFeatures), dtype=np.float64)
+        self._priors = np.zeros(numClasses,dtype=np.float64)
+
+        for i in self._classes:
+            X_i = X[i==y]
+            self._mean[i,:] = X_i.mean(axis=0)
+            self._var[i,:] = X_i.var(axis=0)
+            self._priors[i] = X_i.shape[0] / float(numSamples)
+        
     def predict(self, X):
-        pass
+        y_pred = [self._predict(x) for x in X]
+        return y_pred
+
+    def _predict(self, x):
+        posteriors = []
+
+        for idx, i in enumerate(self._classes):
+            prior = np.log(self._priors[idx])
+            classConditional = np.sum(np.log(self._pdf(idx, x)))
+            posterior = prior + classConditional
+            posteriors.append(posterior)
+
+        return self._classes[np.argmax(posteriors)]
+
+    def _pdf(self, class_idx, x):
+        mean = self._mean[class_idx]
+        var = self._var[class_idx]
+        numerator = np.exp(- (x-mean)**2 / (2*var))
+        denominator = np.sqrt(2* np.pi * var)
+        return numerator/denominator
