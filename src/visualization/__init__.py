c34809ff3610391afbf725150facb888c9d2dc9a
SeanQO <seanquintero3@gmail.com>
2021-06-11 11:25:23 -0500

    added comments

--- src/visualization/__init__.py
+++ src/visualization/__init__.py
@@ -12,13 +12,14 @@ def accuracy(y_true, y_pred):
     accuracy_num = np.sum(y_true == y_pred) / len(y_true)
     return accuracy_num
 
-
 X, y = datasets.make_classification(n_samples=1000, n_features=10, n_classes=2, random_state=123)
 X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=123)
 
 nb = NBClassifier()
 nb.fit(X_train, y_train)
 predictions = nb.predict(X_test)
+
+
 
 print("Naive Bayes classification accuracy", accuracy(y_test, predictions))
 
